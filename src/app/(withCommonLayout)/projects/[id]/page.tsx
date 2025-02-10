c7ac2aa83d4e6642559df577f4c58c7f3ce2cd38
Sanchita Devi <thesanchitadevi@gmail.com>
2025-02-11 04:31:48 +0600

    projects fetched in featured project section

---
+++ src/app/(withCommonLayout)/projects/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDetailPage = () => {
+  return <div>ProjectDetailPage</div>;
+};
+
+export default ProjectDetailPage;
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface IProject {
+  id: string;
+  title: string;
+  description: string;
+  liveLink: string;
+  techStack: string[];
+  image: string;
+}
